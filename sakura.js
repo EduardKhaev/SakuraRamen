f3b57fa9f125cb77fc474e6ba447b5e94af6b9a2
Eduard Puchaew <jamool@gmx.de>
2024-08-13 21:02:45 +0200

    first commit

---
+++ sakura.js
@@ -0,0 +1,23 @@
+function german(){
+    document.getElementById('navabout').innerHTML = 'ÜBER UNS';
+    document.getElementById('navlocations').innerHTML = 'ANFAHRT';
+    document.getElementById('headertext').innerHTML = 'Das beste Ramen in der Stadt';
+    document.getElementById('hto').innerHTML = 'So bestellst du';
+    document.getElementById('pyn').innerHTML = 'Wählen Sie Ihre Nudeln';
+    document.getElementById('pyb').innerHTML = 'Wählen Sie Ihre Brühe';
+    document.getElementById('ayt').innerHTML = 'Fügen Sie Ihre Beilagen';
+    document.getElementById('om').innerHTML = 'Unsere Gerichte';
+    document.getElementById('find-us').innerHTML = 'So finden Sie uns';
+}
+
+function english(){
+    document.getElementById('navabout').innerHTML = 'ABOUT';
+    document.getElementById('navlocations').innerHTML = 'LOCATIONS';
+    document.getElementById('headertext').innerHTML = ' The best Ramen in Town';
+    document.getElementById('hto').innerHTML = ' How To Order ';
+    document.getElementById('pyn').innerHTML = 'Pick your Noodles';
+    document.getElementById('pyb').innerHTML = 'Pick your broth';
+    document.getElementById('ayt').innerHTML = 'Add your toppings';
+    document.getElementById('om').innerHTML = 'Our Menu';
+    document.getElementById('find-us').innerHTML = 'Find Us At';
+}
